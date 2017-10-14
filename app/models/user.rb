4b0f08adf04b59fab4765bee0331b0e58aa663af
kevyn911 <advancerp13@gmail.com>
2017-10-14 16:00:11 +0300

    My lab with registration users

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+	
+	validates :firstname, :lastname, :email, :password, presence: true
+	validates :gender, inclusion: %w(male female)
+	validates :email, uniqueness: true
+	validates :email, uniqueness: { case_sensitive: false }
+	validates :password, length: { in: 6..20 }
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+end
